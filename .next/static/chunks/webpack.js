dddb5200f780682d58547adb05972f75b12b1945
Or Paull <oror18or2@gmail.com>
2026-07-26 15:28:38 +0300

    Add strong CSS selectors for pricing grid mobile responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "32e1f331e64ad4ad"; }
+/******/ 		__webpack_require__.h = function() { return "78aa0d474374b561"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
