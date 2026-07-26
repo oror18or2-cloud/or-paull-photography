9e8675d89d0a6e906be111bbc432276135965f52
Or Paull <oror18or2@gmail.com>
2026-07-26 15:22:43 +0300

    Improve mobile responsive design for pricing cards - force single column layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cdf32619fa760ab6"; }
+/******/ 		__webpack_require__.h = function() { return "a5c4112f6dfbffc5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
